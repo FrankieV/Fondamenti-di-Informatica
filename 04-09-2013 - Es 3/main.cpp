33ff1132afdfc6a6dc188ffb45dff9d12d224666
FrankieV <villellafrancesco@hotmail.it>
2014-05-20 23:05:46 +0200

    Modifica

--- 04-09-2013 - Es 3/main.cpp
+++ 04-09-2013 - Es 3/main.cpp
@@ -5,64 +5,64 @@ const int n = 10;
 
 int main()
 {
-	int A[n];
-	for (int i = 0; i < n; i++)
-	{
-		cin >> A[i];
-	}
+    int A[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> A[i];
+    }
 
-	int corrente = 0;
-	int index = 2;
-	bool trovato = false;
-	int moltiplicando = 0;
-	if (corrente == 0)
-	{
-		while (index < n && trovato == false)
-		{
-			for (int i = index; i < n - 1; i++)
-			{
-				if (A[corrente] == (A[index] * A[i + 1]))
-				{
-					cout << corrente << index << i + 1;
-					trovato = true;
-					break;
-				}
-			}
+    int corrente = 0;
+    int index = 2;
+    bool trovato = false;
+    int moltiplicando = 0;
+    if (corrente == 0)
+    {
+        while (index < n && trovato == false)
+        {
+            for (int i = index; i < n - 1; i++)
+            {
+                if (A[corrente] == (A[index] * A[i + 1]))
+                {
+                    cout << corrente << index << i + 1;
+                    trovato = true;
+                    break;
+                }
+            }
 
-			index++;
+            index++;
 
-		}
-		corrente++;
-		trovato = false;
-	}
-		while (corrente < n)
-		{
-			while (moltiplicando < n && trovato == false)
-			{
-				if (moltiplicando != corrente)
-				{
-					for (int i = 0; i < n; i++)
-					{
-						if (i != moltiplicando && i != corrente)
-						{
-							if (A[corrente] == (A[moltiplicando] * A[i]))
-							{
-								cout << corrente << moltiplicando << i;
-								trovato = true;
-								break;
+        }
+        corrente++;
+        trovato = false;
+    }
+    while (corrente < n)
+    {
+        while (moltiplicando < n && trovato == false)
+        {
+            if (moltiplicando != corrente)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    if (i != moltiplicando && i != corrente)
+                    {
+                        if (A[corrente] == (A[moltiplicando] * A[i]))
+                        {
+                            cout << corrente << moltiplicando << i;
+                            trovato = true;
+                            break;
 
-							}
-						}
-					}
+                        }
+                    }
+                }
 
-					moltiplicando++;
-				}
-				else
-				{
-					moltiplicando++;
-				}
-			}
-            moltiplicando = 0;
-			corrente++;
-		}
+                moltiplicando++;
+            }
+            else
+            {
+                moltiplicando++;
+            }
+        }
+        moltiplicando = 0;
+        corrente++;
+    }
 }
