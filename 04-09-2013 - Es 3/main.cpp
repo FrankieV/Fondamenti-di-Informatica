5805124c7b5e4f574c2f6812539a066e01d44cb0
FrankieV <villellafrancesco@hotmail.it>
2014-06-19 11:52:45 +0200

    Appello d'esame

--- 04-09-2013 - Es 3/main.cpp
+++ 04-09-2013 - Es 3/main.cpp
@@ -1,68 +1,55 @@
 #include <iostream>
-
 using namespace std;
-const int n = 10;
+const int dim = 10;
+void Prodotto_Triple(int A[],int dim,int molt, int pos_1, int pos_2);
 
 int main()
 {
-    int A[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> A[i];
-    }
-
-    int corrente = 0;
-    int index = 2;
-    bool trovato = false;
+    int A[dim] = {1,8,4,3,2,6,5,9,10,11};
     int moltiplicando = 0;
-    if (corrente == 0)
-    {
-        while (index < n && trovato == false)
-        {
-            for (int i = index; i < n - 1; i++)
-            {
-                if (A[corrente] == (A[index] * A[i + 1]))
-                {
-                    cout << corrente << index << i + 1;
-                    trovato = true;
-                    break;
-                }
-            }
+    int pos_1 = 1;
+    int pos_2 = 2;
+    Prodotto_Triple(A,dim,moltiplicando,pos_1,pos_2);
 
-            index++;
+}
 
-        }
-        corrente++;
-        trovato = false;
-    }
-    while (corrente < n)
+void Prodotto_Triple(int A[],int dim,int molt, int pos_1, int pos_2)
+{
+    while(molt < dim)
     {
-        while (moltiplicando < n && trovato == false)
+        while(pos_1 < dim-1)
         {
-            if (moltiplicando != corrente)
+            while(pos_2 < dim)
             {
-                for (int i = 0; i < n; i++)
+                if(pos_1 != molt)
                 {
-                    if (i != moltiplicando && i != corrente)
+                    if(pos_2 != molt && pos_2 != pos_1)
                     {
-                        if (A[corrente] == (A[moltiplicando] * A[i]))
+                        if(A[molt] != A[pos_1] * A[pos_2])
                         {
-                            cout << corrente << moltiplicando << i;
-                            trovato = true;
+                            return Prodotto_Triple(A,dim,molt,pos_1,pos_2+=1);
+                        }
+                        else
+                        {
+                            cout << molt << " " << pos_1 << " " << pos_2 << endl;
+//                            cout << A[molt] << " = " << A[pos_1] << " * " << A[pos_2];
                             break;
-
                         }
                     }
+                    else
+                    {
+                         return Prodotto_Triple(A,dim,molt,pos_1,pos_2+=1);
+                    }
                 }
+                else
+                {
+                     return Prodotto_Triple(A,dim,molt,pos_1+=1,pos_2);
+                }
+            }
 
-                moltiplicando++;
-            }
-            else
-            {
-                moltiplicando++;
-            }
+            return Prodotto_Triple(A,dim,molt,pos_1+=1, pos_2 = pos_1+1);
         }
-        moltiplicando = 0;
-        corrente++;
+
+        return Prodotto_Triple(A,dim,molt+=1, pos_1 = 0, pos_2 = 1);
     }
 }
